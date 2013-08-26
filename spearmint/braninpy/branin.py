0f7183ab83795a35005399a806448a0448cf76fc
Jasper Snoek <jaspersnoek@gmail.com>
2013-08-26 13:12:49 +0200

    A large refactoring - pull request merge from rosejn

--- spearmint/braninpy/branin.py
+++ spearmint/braninpy/branin.py
@@ -4,12 +4,6 @@ import math
 import time
 
 def branin(x):
-  x
-  if x[0] <0 or x[0] > 1:
-    return np.NaN
-
-  if x[1] <0 or x[1] > 1:
-    return np.NaN
 
   x[0] = x[0]*15
   x[1] = (x[1]*15)-5
