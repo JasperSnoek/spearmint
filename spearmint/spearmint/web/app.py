5b115182cc8594225ec595d8b16a334d856af78f
Jasper Snoek <jaspersnoek@gmail.com>
2013-10-02 14:31:06 -0400

    Updated web interface with a display of best parameters

--- spearmint/spearmint/web/app.py
+++ spearmint/spearmint/web/app.py
@@ -1,18 +1,20 @@
 import os
 import sys
 import json
-
+import numpy as np
 from flask import Flask, render_template, redirect, url_for
 
 from spearmint.ExperimentGrid import ExperimentGrid
 from spearmint.helpers import load_experiment
 from spearmint.spearmint_pb2 import _LANGUAGE, _EXPERIMENT_PARAMETERSPEC_TYPE
 
-
 class SpearmintWebApp(Flask):
     def set_experiment_config(self, expt_config):
         self.experiment_config = expt_config
         self.experiment_dir = os.path.dirname(os.path.realpath(expt_config))
+
+    def set_chooser(self, chooser):
+        self.chooser_module = chooser
 
     def experiment_grid(self):
         return ExperimentGrid(self.experiment_dir)
@@ -37,12 +39,31 @@ def status():
     try:
         grid = app.experiment_grid()
         job_ids, scores = app.experiment_results(grid)
+        (best_val, best_job) = grid.get_best()
+        best_params = list()
+        for p in grid.get_params(best_job):
+            best_params.append(p.name + ':')
+            if p.int_val:
+                best_params[-1] += np.array_str(np.array(p.int_val))
+            if p.dbl_val:
+                best_params[-1] += np.array_str(np.array(p.dbl_val))
+        best_params = ','.join(best_params).encode('ascii')
+
+#             for val in p.int_val:
+#                 best_params += str(val) + ','
+#             for val in p.dbl_val:
+#                 best_params += str(val) + ','
+#            best_params += '], '
+
+        #best_params = [' '.join((p.name, str(p.dbl_val), str(p.int_val))) for p in grid.get_params(best_job)]
         stats  = {
-            'candidates': grid.get_candidates().size,
-            'pending':    grid.get_pending().size,
-            'completed':  grid.get_complete().size,
-            'broken':     grid.get_broken().size,
-            'scores':     json.dumps(scores)
+            'candidates':  grid.get_candidates().size,
+            'pending':     grid.get_pending().size,
+            'completed':   grid.get_complete().size,
+            'broken':      grid.get_broken().size,
+            'scores':      json.dumps(scores),
+            'best'  :      best_val,
+            'bestparams' : best_params,
         }
         return render_template('status.html', stats=stats)
     finally:
@@ -53,6 +74,10 @@ def status():
 
 @app.route("/")
 def home():
+    #grid = app.experiment_grid()
+    #job_ids, scores = app.experiment_results(grid)
+    #(best_val, best_job) = grid.get_best()    
+    #best_params = grid.get_params(best_job)
     exp = app.experiment()
     params = []
     for p in exp.variable:
@@ -62,16 +87,15 @@ def home():
             'max': p.max,
             'type': _EXPERIMENT_PARAMETERSPEC_TYPE.values_by_number[p.type].name,
             'size': p.size
-        }
+            }
         params.append(param)
 
     ex = {
         'name': exp.name,
         'language': _LANGUAGE.values_by_number[exp.language].name,
         'params': params
-    }
+        }
     return render_template('home.html', experiment=ex)
-
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
